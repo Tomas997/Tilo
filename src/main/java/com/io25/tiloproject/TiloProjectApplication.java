35cd6f2e7cee37df9cf18e3e73dabfd33537e98c
Andriy Plaksenkov <plaksenkov.andriy@lll.kpi.ua>
2024-04-20 22:31:53 +0300

    init commit

---
+++ src/main/java/com/io25/tiloproject/TiloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.io25.tiloproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiloProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiloProjectApplication.class, args);
+    }
+
+}
